5a56e263a2ee20b9c728e3e086736113d9fd5a79
HARSH SHARMA <60179664+hrs2001sharma@users.noreply.github.com>
2020-11-13 22:33:04 +0530

    Phone Bar and Activity Bar
    Phone Bar - Transparent
    Activity Bar-Black

--- app/src/main/java/com/dev/ieeensut/MainActivity.java
+++ app/src/main/java/com/dev/ieeensut/MainActivity.java
@@ -2,13 +2,24 @@ package com.dev.ieeensut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.black)));
     }
 }
