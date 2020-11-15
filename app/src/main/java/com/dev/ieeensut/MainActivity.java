87afa249b4ec1215f5fe3322e4f6aeea057d357d
HARSH SHARMA <60179664+hrs2001sharma@users.noreply.github.com>
2020-11-15 15:46:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/dev/ieeensut/MainActivity.java
+++ app/src/main/java/com/dev/ieeensut/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setStatusBarColor(getColor(R.color.material_on_surface_emphasis_high_type));
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.black)));getWindow().setStatusBarColor(getColor(R.color.material_on_surface_emphasis_high_type));
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.black)));
     }
 }
