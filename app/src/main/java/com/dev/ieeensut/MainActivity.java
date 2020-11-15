57a5082890fa395b41f5fa7990bdfe99ff2a5ff9
HARSH SHARMA <60179664+hrs2001sharma@users.noreply.github.com>
2020-11-15 15:15:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/dev/ieeensut/MainActivity.java
+++ app/src/main/java/com/dev/ieeensut/MainActivity.java
@@ -3,11 +3,8 @@ package com.dev.ieeensut;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
